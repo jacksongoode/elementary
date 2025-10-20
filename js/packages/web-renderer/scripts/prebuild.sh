23e650ec61010966612da5fea070e51affafa751
Jackson Goode <54308792+jacksongoode@users.noreply.github.com>
2025-10-20 17:36:39 +0900

    Too?

--- js/packages/web-renderer/scripts/prebuild.sh
+++ js/packages/web-renderer/scripts/prebuild.sh
@@ -3,10 +3,10 @@
 set -x
 set -e
 
-# ROOT_DIR="$(git rev-parse --show-toplevel)"
-# CURRENT_DIR="$(pwd)"
-#
-#
-# pushd "$ROOT_DIR"
-# ./scripts/build-wasm.sh -o "$CURRENT_DIR/raw/elementary-wasm.js"
-# popd
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+CURRENT_DIR="$(pwd)"
+
+
+pushd "$ROOT_DIR"
+./scripts/build-wasm.sh -o "$CURRENT_DIR/raw/elementary-wasm.js"
+popd
